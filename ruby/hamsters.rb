b5c6279e82bddfd4201fda2c474b6f51729f5999
Ilya Yampolsky <cfnils@gmail.com>
2016-03-23 16:01:17 -0400

    release 1 gather data on hamsters

---
+++ ruby/hamsters.rb
@@ -0,0 +1,13 @@
+puts "hamster's name?"
+name = gets.chomp
+puts "volume level (1-10)?"
+volume = gets.chomp
+puts "fur color?"
+fur_color = gets.chomp
+puts "makes a good adoption candidate?"
+adoption = gets.chomp
+puts "estimated age?"
+age = gets.chomp
+if age == ""
+	age = nil
+end
