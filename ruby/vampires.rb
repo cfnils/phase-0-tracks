a2c5c4c8711f6d2624f3830071c45c997bdb3bf3
Ilya Yampolsky <cfnils@gmail.com>
2016-03-24 13:15:10 -0400

    release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
+
+
+# case grade
+# when "A", "B"
+#   puts 'You pretty smart!'
+# when "C", "D"
+#   puts 'You pretty dumb!!'
+# else
+#   puts "You can't even use a computer!"
+# end
