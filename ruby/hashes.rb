cb224095b384ef3e02948f04529e95b5491c0872
Ilya Yampolsky <cfnils@gmail.com>
2016-03-29 19:12:17 -0400

    5.3 add code for release 3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,16 +1,48 @@
-#prompt user for details
+#prompt user for details, and convert inputs to the correct data types if necessary
+puts "Hello new client! Please let us know the following information"
 
-#convert inputs to the correct data types
+puts "Please enter your name:"
+name = gets.chomp
+
+puts "Please enter your age:"
+age = gets.chomp.to_i
+
+puts "Do you have any children? Please enter 'true' or 'false':"
+children = gets.chomp
+
+if children == "true"
+	children = true
+else
+	children = false
+end
+
+
+puts "Please enter your decor theme:"
+theme = gets.chomp
+
+#create the hash
+new_client = {client_name: name, client_age: age, client_has_children: children, client_theme: theme}
 
 #print out the hash on screen
+puts new_client
 
-#ask user if they have a new input
+#ask user which key they would like to update. get input and convert to symbol data type
+puts "Which key you would like to update? Please enter key name or none if you don't need to:"
+update = gets.chomp
 
 #if they don't input "none" 
-	#convert input to symbol 
+	#convert string to symbol
 	#ask for value
-	#assign the value to a new key with the above symbol for a name
+	#assign the value to the key
+if update != "none"
+	update = update.to_sym
+	puts "New Value:"
+	new_value = gets.chomp
+	new_client[update] = new_value
+end
 
 #print out the new version of the hash
+puts new_client
 
 #exit program
+exit
