f70a2fb0f10a31f60e2199576cb368b51b6ab987
Ilya Yampolsky <cfnils@gmail.com>
2016-03-29 16:17:24 -0400

    5.3 psuedocoding

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+#prompt user for details
+
+#convert inputs to the correct data types
+
+#print out the hash on screen
+
+#ask user if they have a new input
+
+#if they don't input "none" 
+	#convert input to symbol 
+	#ask for value
+	#assign the value to a new key with the above symbol for a name
+
+#print out the new version of the hash
+
+#exit program
