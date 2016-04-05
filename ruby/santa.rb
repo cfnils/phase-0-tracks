616e8c96f97d495c489c8f48b5c1730e32451247
Ilya Yampolsky <cfnils@gmail.com>
2016-04-05 16:24:09 -0400

    release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
-	
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance"
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+
 	def speak
 		puts "Ho, ho, ho! Haaappy holidays!"
 	end
@@ -8,11 +16,16 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize
-		puts "Initializing Santa instance"
+	def about
+		puts "gender is #{@gender}"
+		puts "ethnicity is #{@ethnicity}"
+		puts "reindeer_ranking is #{@reindeer_ranking}"
+		puts "age is #{@age}"
 	end
+
 end
 
-doofus = Santa.new
+doofus = Santa.new("male", "Asian")
 doofus.speak
 doofus.eat_milk_and_cookies("chocolate chip cookie")
+doofus.about
