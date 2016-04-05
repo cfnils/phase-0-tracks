27dcdab3e766cd0c58f1f0201b5b7aa918f08353
Ilya Yampolsky <cfnils@gmail.com>
2016-04-05 15:48:10 -0400

    release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,18 @@
+class Santa
+	
+	def speak
+		puts "Ho, ho, ho! Haaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance"
+	end
+end
+
+doofus = Santa.new
+doofus.speak
+doofus.eat_milk_and_cookies("chocolate chip cookie")
