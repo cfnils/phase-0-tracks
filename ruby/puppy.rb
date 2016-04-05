afec7d5f238e984bb5f10af143f083e2a669c626
Ilya Yampolsky <cfnils@gmail.com>
2016-04-05 15:15:16 -0400

    release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES -----------
+# Canis lupus familiaris
+
+# CHARACTERISTICS --------
+# Legs: 4
+# Toungue: long
+# Snout: long
+# Name: varies
+# Color: varies
+
+# BEHAVIOR ------------
+# Play
+# Bark
+# Fetch
