f96f7bd864642862f2c22427fc3295a9983fb7f6
Ilya Yampolsky <cfnils@gmail.com>
2016-04-07 20:23:14 -0400

    release 0, psuedocode the class design

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+# Set up class for dragon
+# set attributes for dragon
+# 	name
+# 	age
+# 	if its evil (true or false)
+# set methods for dragon
+# 	fly to someplace- takes destination as arguement
+# 	breathe fire
+# 	watch tv in cave- takes name of show as arguement
+# 	
