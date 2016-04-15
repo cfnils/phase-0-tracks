08c59d66936fc5426d09cb14bdd6e4726ef716a2
Ilya Yampolsky <cfnils@gmail.com>
2016-04-14 20:08:26 -0400

    release 1

--- js/algos.js
+++ js/algos.js
@@ -7,17 +7,33 @@
 // 	return the value at the longIndex for the array
 
 function longestElement(array) {
-	longest = 0;
+	var longest = 0;
 	for (var i = 0; i < array.length; i++) {
 		if (array[i].length > longest) {
-			longIndex = i;
+			var longIndex = i;
 			longest = array[i].length;
 		}
 	}
+	console.log(longest);
 	return array[longIndex];
 }
 
+
+function samePair(object1, object2) {
+	var match = false;
+	for (var i in object1) {
+		if (object1[i] == object2[i]) {
+			match = true;
+		}
+	}
+	return match;
+}
+
+
+// Driver Code
 console.log(longestElement(["long phrase", "longest phrase", "longer phrase"]));
-console.log(longest);
 console.log(longestElement(["i don't know", "oh ok", "what"]));
-console.log(longest);
+
+console.log(samePair({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(samePair({name: "Kevin", age: 99}, {name: "John", age: 44, weight: 110}));
+
