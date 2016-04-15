dec0889df1b3b76e1c7f60abe6666aadd1503059
Ilya Yampolsky <cfnils@gmail.com>
2016-04-14 22:30:04 -0400

    release 2

--- js/algos.js
+++ js/algos.js
@@ -18,6 +18,9 @@ function longestElement(array) {
 	return array[longIndex];
 }
 
+//define function that takes 2 objects as arguements
+// for every property in the first object, see if there is the same one in the second object
+	//if there is, return true
 
 function samePair(object1, object2) {
 	var match = false;
@@ -29,6 +32,29 @@ function samePair(object1, object2) {
 	return match;
 }
 
+//define funtion that takes an integer as an arguement
+//for as many times as the integer
+	//for as many times as a random number between 1 and 10
+		//generate a random letter and add it to a string
+	//store the strings in an array
+
+
+function generator(int){
+	var arrizay = [];
+	for (var i = 0; i < int; i++) {
+		var possible = "abcdefghijklmnopqrstuvwxyz"
+		var stringLength = Math.floor((Math.random() * 10) + 1);
+		var stringle = '';
+		for (var x = 0; x < stringLength; x++) {
+			
+			stringle += possible.charAt(Math.random() * 26);
+		}
+		arrizay.push(stringle);
+	}
+	return arrizay;
+}
+
+
 
 // Driver Code
 console.log(longestElement(["long phrase", "longest phrase", "longer phrase"]));
@@ -36,4 +62,10 @@ console.log(longestElement(["i don't know", "oh ok", "what"]));
 
 console.log(samePair({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
 console.log(samePair({name: "Kevin", age: 99}, {name: "John", age: 44, weight: 110}));
+
+for (var i = 0; i < 10; i++) {
+	var genArray = generator(4);
+	console.log(genArray);
+	console.log(longestElement(genArray));
+}
 
